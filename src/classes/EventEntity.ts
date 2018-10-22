3bf674d00a68bfd5f53295aa9ce82a91d8d6aa4d
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2018-10-22 17:11:59 -0300

    Update EventEntity state and properties automagically

--- src/classes/EventEntity.ts
+++ src/classes/EventEntity.ts
@@ -2,6 +2,7 @@ import { IEvent, Reducer, ICommitFunction } from '@nxcd/tardis'
 import { IEventEntity } from '../interfaces/IEventEntity'
 
 export abstract class EventEntity<TEntity> implements IEventEntity {
+  [ key: string ]: any
   persistedEvents: IEvent<any>[] = []
   pendingEvents: IEvent<any>[] = []
   id: any = null
@@ -16,13 +17,21 @@ export abstract class EventEntity<TEntity> implements IEventEntity {
     throw new Error('Method not implemented.')
   }
 
+  private updateState () {
+    for (const propertyName of this.state) {
+      this[propertyName] = this.state[propertyName]
+    }
+  }
+
   setPersistedEvents(events: IEvent<any>[]) {
     this.persistedEvents = events
+    this.updateState()
     return this
   }
 
   pushNewEvents(events: IEvent<any>[]) {
     this.pendingEvents = this.pendingEvents.concat(events)
+    this.updateState()
     return this
   }
 
