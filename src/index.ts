d365ac6d84a54bc429c1c47be4e97469bf8db1b2
Rogério Munhoz <rogerio.munhoz@nxcd.com.br>
2018-10-30 17:49:30 -0300

    Addd bulkUpdate and _runPaginatedQuery to MongodbEventRepository

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { EventEntity } from './classes/EventEntity'
 export { IEventEntity } from './interfaces/IEventEntity'
 
 export { IEventRepository } from './interfaces/IEventRepository'
+export { IPaginatedQueryResult } from './interfaces/IPaginatedQueryResult'
 export { MongodbEventRepository } from './classes/repositories/MongodbEventRepository'
 
 export { Event, Reducer, IEvent, ICommitFunction } from '@nxcd/tardis'
