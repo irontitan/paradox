ee96c7b3df14460cb4e74cf5494ddb26487cb636
Lucas Santos <lhs.santoss@gmail.com>
2019-01-29 14:33:37 -0200

    Update EventRepository.ts

--- src/classes/repositories/EventRepository.ts
+++ src/classes/repositories/EventRepository.ts
@@ -15,7 +15,7 @@ export abstract class EventRepository<TEntity extends IEventEntity> {
 
   abstract async findById (id: any): Promise<TEntity | null>
     
-  abstract async existsBy (query: { [key: string]: any }): Promise<boolean>
+  abstract async existBy (query: { [key: string]: any }): Promise<boolean>
 
   abstract async _runPaginatedQuery (query: { [key: string]: any }, page: number, size: number, sort: { [field: string]: 1 | -1 }): Promise<IPaginatedQueryResult<{ events: IEvent<TEntity>[] }>>
 }
